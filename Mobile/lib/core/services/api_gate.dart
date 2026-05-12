c68d3f661e136d5209dbea2f1c2d38a5fb53ab4e
Nailyaa <nailyaa225@gmail.com>
2026-05-12 12:06:27 +0700

    update v2 mahasiswa

--- Mobile/lib/core/services/api_gate.dart
+++ Mobile/lib/core/services/api_gate.dart
@@ -1,7 +1,8 @@
 class ApiGate {
-  /// Ubah IP Address di sini saja untuk menyelaraskan Mobile dan Backend
-  /// Contoh: http://192.168.18.65:8000/api
-  static const String baseUrl = 'http://localhost:8000/api';
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: 'http://localhost:8000/api',
+  );
 
   static const String environment = 'development';
 }
